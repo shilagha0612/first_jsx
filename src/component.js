720379322c6b492508ee7530b29d3aff297df1a9
shilagha0612 <sshilagha786@gmail.com>
2023-06-07 18:24:38 -0400

    FIRST JSX FILE IN REACT

---
+++ src/component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Component() {
+    return (
+        <div>
+            <h2>This is a custom component</h2>
+            <p>Custom component content goes here.</p>
+        </div>
+    );
+}
+
+export default Component;
