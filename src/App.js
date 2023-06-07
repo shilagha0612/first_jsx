885288b99188970f8ede4c55abe5875612d3b746
shilagha0612 <sshilagha786@gmail.com>
2023-06-07 18:24:11 -0400

    First Jsx file in React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Component from './component.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Dojo</h1>
+      <h3>Things I need to do: </h3>
+      <ul>
+        <li>Learn React</li>
+        <li>Build a React app</li>
+        <li>Deploy the React app</li>
+      </ul>
+      <Component />
     </div>
   );
 }
+
 
 export default App;
